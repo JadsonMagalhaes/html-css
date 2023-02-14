06f3a17d00b2438f59b3a54561877fa0241bd8fe
JadsonMagalhaes <116447428+JadsonMagalhaes@users.noreply.github.com>
2023-02-14 11:09:43 -0300

    Initial commit

---
+++ Exercicios/ex010/livro/Update 15.12.2022/Cosmoss/licença.php
@@ -0,0 +1,17 @@
+<?php
+//Essa e a sua nova licença e o HWID criptofrado e conectado ao hack.
+//Caso seja compartilhado o sistema vai identificar e fazer o bloqueio automático.
+// Se você quiser liberar em um novo computador entre em contato.
+// Caso queira que outros amigos usem tbm, me avise que faço um desconto.
+
+function conding($generate){
+	//Licença de uso.
+	$lincence = "86d42890-bf01-4cdf-a9f4-d253d83a1gaa";
+	
+	//Coleta o número de serial do seu HWID.
+	$HWID = generate($arquivo, "YduwqYhnOPJvy4bczL1NVaJAsSJzDTUAmrkeExCpgLk=");
+
+}
+
+
+?>
